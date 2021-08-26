50dc4ca0dbe4ef243af34f932a0a818085370789
Ignacio Wolf <ignaciowolf10@gmail.com>
2021-08-26 19:55:36 -0300

    Entregable 2

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-let nombre = prompt("Ingrese su nombre");
-let apellido = prompt("Ingrese su apellido");
-let salida = "Hola " + nombre + " " + apellido;
-alert(salida);
+let nombre = prompt("Ingrese su nombre:");
+let apellido = prompt("Ingrese su apellido:");
+let edad = prompt ("Ingrese su edad:");
+
+if(parseInt(edad)<18){
+    alert("Hola " + nombre + " " + apellido + ", debes consultar con un mayor para acceder a este sitio.");
+} 
+else{
+    alert("Hola " + nombre + " " + apellido + ", eres mayor de edad.");
+}
