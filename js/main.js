878d51f0b3b3e8c837c3d04ebe794470ec22fd41
Ignacio Wolf <ignaciowolf10@gmail.com>
2021-09-27 13:43:44 -0300

    Segunda Pre-entrega

--- js/main.js
+++ js/main.js
@@ -14,17 +14,27 @@ class Fotocopiadora {
         }
     }
 }
-const fotocopiadoras = [];
 
-const section = document.getElementById("section");
-let crearRegistro = document.getElementById("registro");
-crearRegistro.addEventListener("submit",crearFotocopiadora);
+function setFotocopiadora(fotocopiadoras) {
+    localStorage.setItem("fotocopiadoras",JSON.stringify(fotocopiadoras));
+}
+
+function imprimirFotocopiadora(fotocopiadoras) {
+    for (const fotocopiadora of fotocopiadoras) {
+        let div = document.createElement("div");
+        div.innerHTML = `<h3> Marca: ${fotocopiadora.marca}</h3>
+                            <h3> Modelo: ${fotocopiadora.modelo}</h3>
+                            <h3> Estado: ${fotocopiadora.stock}</h3>
+                            <h3> Stock: ${fotocopiadora.estado}</h3>
+                            <br>`;
+        section.appendChild(div);   
+    }
+}
 
 function crearFotocopiadora(event) {
     event.preventDefault();
     const inputs = document.getElementsByClassName("crearFotocopiadora");
     fotocopiadoras.push(new Fotocopiadora(inputs[0].value,inputs[1].value,inputs[2].value,inputs[3].value));
-    console.log(fotocopiadoras);
     let div = document.createElement("div");
     div.innerHTML = `<h3> Marca: ${inputs[0].value}</h3>
                             <h3> Modelo: ${inputs[1].value}</h3>
@@ -32,4 +42,22 @@ function crearFotocopiadora(event) {
                             <h3> Stock: ${inputs[3].value}</h3>
                             <br>`;
     section.appendChild(div);
+    setFotocopiadora(fotocopiadoras);
 }
+
+
+const section = document.getElementById("section");
+let crearRegistro = document.getElementById("registro");
+
+const almacenados = JSON.parse(localStorage.getItem("fotocopiadoras"));
+const fotocopiadoras = [];
+
+if (localStorage.length != 0) {
+    for (const objeto of almacenados) {
+        fotocopiadoras.push(new Fotocopiadora(objeto.marca,objeto.modelo,objeto.stock,objeto.estado));
+    }
+    imprimirFotocopiadora(fotocopiadoras); 
+}
+
+crearRegistro.addEventListener("submit",crearFotocopiadora);
+
